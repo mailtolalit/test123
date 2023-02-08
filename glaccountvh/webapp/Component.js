0dd26c231bdfdb27fcfd6a654502c5ef1c02eda7
Lalit Gupta <lalit.gupta@kcc.com>
2023-02-08 10:56:11 +0000

    first commit

---
+++ glaccountvh/webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("com.kc.glaccountvh.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
